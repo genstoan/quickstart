f9072452d50d1b2745d628136e2762f596132efd
Niranjan Rath <genstoan@gmail.com>
2016-08-07 09:25:52 +0200

    First Project

---
+++ src/main/java/com/niranjan/quickstart/App.java
@@ -0,0 +1,10 @@
+package com.niranjan.quickstart;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
